a4322954be33bc64d58bf9dc475e294f38cc56ea
1207862771@qq.com <1207862771@qq.com>
2018-08-29 16:49:45 +0800

    新增了一个异常检查

---
+++ demo/first.py
@@ -0,0 +1,17 @@
+a = 'abca'
+b='a1234'
+c=a+b
+print(c.find('4'))
+list1=c.split('c')
+list2=[1,2,1,1,1]
+list1.extend(list2)
+print(list1)
+i=1
+try:
+    if i==1:
+        for i in range(10):
+            print(i)
+    elif i==2:
+            print(i+1)
+except:
+    print('sorry,input error')
---
+++ first_javascript.js
@@ -0,0 +1,23 @@
+
+
+//    var a=prompt("用户输入一个名称：");
+//   if(a=="admin"){
+//       document.write("用户正确");
+//   }else{
+//       document.write("用户错误");
+//   }
+
+    var a=prompt("输入一个名称：");
+   if(a=="admin")
+   {var b=prompt("请输入你的密码：");
+      if(b=="12345"){
+          document.write("登陆成功")}
+          else{ document.write("登陆失败")}
+      }
+   else {
+       document.write("账户错误");
+   }
+   i-=1
+   for i in range(1000):
+     print(i)
+
